7c7e2be5692494a77ea1e49311f94594c47b0d01
Saad Khan <73062453+SaadKhan-22@users.noreply.github.com>
2025-12-11 18:41:13 +0500

    Added JOINs and Stuff

--- definitive_spark_guide/Bruh.py
+++ definitive_spark_guide/Bruh.py
@@ -538,3 +538,49 @@ retailDfMod.select(
 
 # COMMAND ----------
 
+# DBTITLE 1,JOINs
+person = spark.createDataFrame([
+ (0, "Bill Chambers", 0, [100]),
+  (1, "Matei Zaharia", 1, [500, 250, 100]),
+ (2, "Michael Armbrust", 1, [250, 100])]) \
+ .toDF("id", "name", "graduate_program", "spark_status")
+
+graduateProgram = spark.createDataFrame([
+ (0, "Masters", "School of Information", "UC Berkeley"),
+ (2, "Masters", "EECS", "UC Berkeley"),
+ (1, "Ph.D.", "EECS", "UC Berkeley")]) \
+ .toDF("id", "degree", "department", "school")
+
+sparkStatus = spark.createDataFrame([
+ (500, "Vice President"),
+ (250, "PMC Member"),
+ (100, "Contributor")]) \
+ .toDF("id", "status")
+
+
+
+person.createOrReplaceTempView("person")
+graduateProgram.createOrReplaceTempView("graduateProgram")
+sparkStatus.createOrReplaceTempView("sparkStatus")
+
+# COMMAND ----------
+
+leftJoinExpression = person['graduate_program'] == graduateProgram['id']
+
+# COMMAND ----------
+
+"""
+person.join(graduateProgram, leftJoinExpression, 'left' ).show()
+
+person.join(graduateProgram, leftJoinExpression, 'full_outer' ).show()
+
+person.join(graduateProgram, leftJoinExpression, 'right' ).show()
+
+person.join(graduateProgram, leftJoinExpression, 'cross' ).show()
+
+"""
+#person.join(graduateProgram, leftJoinExpression, 'cross' ).count()
+person.crossJoin(graduateProgram).count()
+
+# COMMAND ----------
+
